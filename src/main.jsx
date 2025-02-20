b97ed4d7f63a6f7bff9fc28552b1a1f2e6e77e95
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-02-20 19:31:37 +0600

    Added Context API and Firebase Othentication

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import { router } from './routes/Routes';
+import { router } from "./routes/Routes";
+import APIcontext from "./utilities/context/APIcontext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-   <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <APIcontext>
+      <RouterProvider router={router} />
+    </APIcontext>
+  </StrictMode>
+);
---
+++ src/utilities/context/APIcontext.jsx
@@ -0,0 +1,109 @@
+import PropTypes from "prop-types";
+import ContextProvider from "./ContextProvider";
+import auth from "../firebase/firebase.config";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+
+const APIcontext = ({ children }) => {
+  const API_LINK = import.meta.env.VITE_API_LINK;
+  const [isDark, setIsDark] = useState(false);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const googleProvider = new GoogleAuthProvider();
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const updateUser = (userInfo) => {
+    return updateProfile(auth.currentUser, userInfo);
+  };
+
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const signOutUser = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+
+      // send jwt token
+      const sendEmailToMakeJWTtoken = async () => {
+        const user = { email: currentUser?.email };
+        try {
+          await axios.post(`${API_LINK}/jwt`, user, { withCredentials: true });
+        } catch (err) {
+          Swal.fire({
+            title: "Error",
+            text: err.message,
+            icon: "error",
+            confirmButtonText: "Okay",
+          });
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      // delete jwt token
+      const deleteToken = async () => {
+        try {
+          await axios.delete(`${API_LINK}/logout`, { withCredentials: true });
+        } catch (err) {
+          Swal.fire({
+            title: "Error",
+            text: err.message,
+            icon: "error",
+            confirmButtonText: "Okay",
+          });
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      if (currentUser?.email) sendEmailToMakeJWTtoken();
+      else deleteToken();
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const value = {
+    isDark,
+    setIsDark,
+    user,
+    setUser,
+    loading,
+    setLoading,
+    createUser,
+    updateUser,
+    googleSignIn,
+    signOutUser,
+  };
+  return (
+    <ContextProvider.Provider value={value}>
+      {children}
+    </ContextProvider.Provider>
+  );
+};
+
+APIcontext.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default APIcontext;
