0982a2a186df23c73ba9e7c5d7da66adc053cddc
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-02-20 13:12:32 +0600

    Created Files and Folders, and basic codes

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,11 @@
+
+
+const MainLayout = () => {
+    return (
+        <div className="bg-red-600">
+            I am working......            
+        </div>
+    );
+};
+
+export default MainLayout;
