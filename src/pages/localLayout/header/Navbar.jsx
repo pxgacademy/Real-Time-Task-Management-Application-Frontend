bd416cf07b00145759cf3e0f9a028bce5e85ae0b
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-02-24 21:15:33 +0600

    Updated

--- src/pages/localLayout/header/Navbar.jsx
+++ src/pages/localLayout/header/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="border-b border-white/50 py-5 px-5 md:px-7 lg:px-14 flex items-center justify-between">
+    <nav className="border-b border-white/50 py-5 px-5 md:px-7 lg:px-14 flex flex-col md:flex-row gap-5 items-center justify-between">
       <div>
         <Link
           to="/visitor"
@@ -14,12 +14,12 @@ const Navbar = () => {
 
       <div>
         <Link to="/visitor/login" className="mr-5">
-          <button className="border border-transparent py-3 px-10 font-semibold text-xl rounded-lg hover:border-white transition-all duration-200 cursor-pointer">
+          <button className="border border-transparent p-2 md:py-3 md:px-10 font-semibold text-xl rounded-lg hover:border-white transition-all duration-200 cursor-pointer">
             Login
           </button>
         </Link>
         <Link to="/visitor/register">
-          <button className="py-3 px-10 font-semibold text-xl rounded-lg bg-rose-500 hover:scale-105 transition-all duration-200 cursor-pointer">
+          <button className="p-2 md:py-3 md:px-10 font-semibold text-xl rounded-lg bg-rose-500 hover:scale-105 transition-all duration-200 cursor-pointer">
             Register for Free
           </button>
         </Link>
